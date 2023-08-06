c6c7f211bfea769ae4a93643eaf22c5d7a87a735
va7ul <vas7ul@gmail.com>
2023-08-06 17:34:53 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import user from '../user.json;
+import { Profile } from './Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile username={user.username}/>
     </div>
   );
 };
