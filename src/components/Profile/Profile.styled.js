74efe1b88a8686c49265384e61c82766305a5e52
va7ul <vas7ul@gmail.com>
2023-08-07 14:12:54 +0300

    Update Profile.styled.js

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -2,7 +2,7 @@ import { styled } from 'styled-components';
 
 export const Card = styled.div`
   width: 300px;
-  margin: 0 auto;
+  margin: 50px auto 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
